cf0782c2e4b5899110ced7762accdf5eb14a8baa
amulls <amanda.l.mullings@gmail.com>
2025-06-23 22:42:50 -0400

    Update CustomizeProducts.tsx

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -116,30 +116,7 @@ const CustomizeProducts = ({
         }
         stockNumber={selectedVariant?.stock?.quantity || 0}
       />
-      {/* COLOR */}
-      {/* 
-          <ul className="flex items-center gap-3">
-            <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-red-500">
-              <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-            </li>
-            <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-blue-500"></li>
-            <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-not-allowed relative bg-green-500">
-              <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-            </li>
-          </ul> */}
-      {/* OTHERS */}
-      {/* <h4 className="font-medium">Choose a size</h4>
-      <ul className="flex items-center gap-3">
-        <li className="ring-1 ring-lama text-lama rounded-md py-1 px-4 text-sm cursor-pointer">
-          Small
-        </li>
-        <li className="ring-1 ring-lama text-white bg-lama rounded-md py-1 px-4 text-sm cursor-pointer">
-          Medium
-        </li>
-        <li className="ring-1 ring-pink-200 text-white bg-pink-200 rounded-md py-1 px-4 text-sm cursor-not-allowed">
-          Large
-        </li>
-      </ul> */}
+      {/* instead of looping through with the type for each item, create button or selecting bullet point or checkbox for ebook page to select regular or vegan diet, or just make two separate items (one vegan, one regular), color scheme of self-care kit, size of a kit possibly or what key things you want to have in the kit */}
     </div>
   );
 };
