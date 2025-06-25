18d3dbeea732ccfbef4aae7a32a92f11071c5efd
amulls <amanda.l.mullings@gmail.com>
2025-06-25 01:20:53 -0400

    Update Filter.tsx

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -22,7 +22,7 @@ const Filter = () => {
         <select
           name="type"
           id=""
-          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+          className="py-2 px-4 rounded-2xl text-xs font-medium bg-gray-200"
           onChange={handleFilterChange}
         >
           <option>Type</option>
