491beb79505a8c0a6c0805bed0b79f93f750a4ec
amulls <amanda.l.mullings@gmail.com>
2025-06-26 01:48:40 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,39 +3,39 @@ import Menu from "./Menu";
 import Image from "next/image";
 import SearchBar from "./SearchBar";
 import dynamic from "next/dynamic";
-// import NavIcons from "./NavIcons";
 
 const NavIcons = dynamic(() => import("./NavIcons"), { ssr: false });
 
 const Navbar = () => {
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+    <div className="fixed top-7 left-0 w-full h-20 p-10 bg-white shadow-md z-[1000] flex items-center transition-all duration-500">
       {/* MOBILE */}
-      <div className="h-full flex items-center justify-between md:hidden">
+      <div className="h-full flex items-center justify-between w-full md:hidden">
         <Link href="/">
-          <div className="text-2xl tracking-wide">LAMA</div>
+          <div className="text-2xl tracking-wide">ONWARD</div>
         </Link>
         <Menu />
       </div>
       {/* BIGGER SCREENS */}
-      <div className="hidden md:flex items-center justify-between gap-8 h-full">
+      <div className="hidden md:flex items-center justify-between gap-8 h-full w-full">
         {/* LEFT */}
         <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
-          <Link href="/" className="flex items-center gap-3">
-            <Image src="/logo.png" alt="" width={24} height={24} />
-            <div className="text-2xl tracking-wide">LAMA</div>
+          <Link href="/" className="flex items-center gap-3 hover:scale-105">
+            <Image src="/age-group.png" alt="logo" width={24} height={24} />
+            <div className="text-2xl tracking-wide">ONWARD</div>
           </Link>
           <div className="hidden xl:flex gap-4">
-            <Link href="/">Homepage</Link>
-            <Link href="/">Shop</Link>
-            <Link href="/">Deals</Link>
-            <Link href="/">About</Link>
-            <Link href="/">Contact</Link>
+            <Link href="/list?cat=all-products" className="hover:scale-105">Shop All</Link>
+            <Link href="/" className="hover:scale-105">About</Link>
+          </div>
+          <div className="flex hover:scale-105 xl:hidden">
+            <Link href="/list?cat=all-products" className="hover:scale-105">
+              <img src="/shop-now.png" alt="shop" width={65} height={55}/>
+            </Link>
           </div>
         </div>
         {/* RIGHT */}
-        <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
-          <SearchBar />
+        <div className="w-2/3 xl:w-1/2 flex items-center justify-end gap-8">
           <NavIcons />
         </div>
       </div>
