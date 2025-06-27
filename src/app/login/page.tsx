1bf17009544b547113084687e22df1abf80cb4fa
amulls <amanda.l.mullings@gmail.com>
2025-06-26 20:39:54 -0400

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { useWixClient } from "@/hooks/useWixClient";
 import { LoginState } from "@wix/sdk";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import Cookies from "js-cookie";
 import { useState } from "react";
 
@@ -33,6 +33,8 @@ const LoginPage = () => {
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
 
+  const pathname = usePathname();
+  
   const formTitle =
     mode === MODE.LOGIN
       ? "Log in"
@@ -74,11 +76,11 @@ const LoginPage = () => {
           });
           break;
         case MODE.RESET_PASSWORD:
-          response = await wixClient.auth.sendPasswordResetEmail(
+          response = await wixClient.auth.sendPasswordResetEmail({
             email,
             window.location.href
-          );
-          setMessage("Password reset email sent. Please check your e-mail.");
+          });
+          setMessage("An email has been sent with instructions to reset your password.");
           break;
         case MODE.EMAIL_VERIFICATION:
           response = await wixClient.auth.processVerification({
@@ -109,16 +111,16 @@ const LoginPage = () => {
           ) {
             setError("Invalid email or password!");
           } else if (response.errorCode === "emailAlreadyExists") {
-            setError("Email already exists!");
+            setError("An account is already registered to this email.");
           } else if (response.errorCode === "resetPassword") {
-            setError("You need to reset your password!");
+            setError("You need to reset your password.");
           } else {
             setError("Something went wrong!");
           }
         case LoginState.EMAIL_VERIFICATION_REQUIRED:
           setMode(MODE.EMAIL_VERIFICATION);
         case LoginState.OWNER_APPROVAL_REQUIRED:
-          setMessage("Your account is pending approval");
+          setMessage("Your account is pending approval.");
         default:
           break;
       }
@@ -131,7 +133,7 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="h-[calc(100vh-80px)] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex items-center justify-center">
+    <div className="h-[calc(100vh)] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex items-center justify-center">
       <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
         <h1 className="text-2xl font-semibold">{formTitle}</h1>
         {mode === MODE.REGISTER ? (
@@ -140,7 +142,7 @@ const LoginPage = () => {
             <input
               type="text"
               name="username"
-              placeholder="john"
+              placeholder="Username"
               className="ring-2 ring-gray-300 rounded-md p-4"
               onChange={(e) => setUsername(e.target.value)}
             />
@@ -148,11 +150,11 @@ const LoginPage = () => {
         ) : null}
         {mode !== MODE.EMAIL_VERIFICATION ? (
           <div className="flex flex-col gap-2">
-            <label className="text-sm text-gray-700">E-mail</label>
+            <label className="text-sm text-gray-700">Email</label>
             <input
               type="email"
               name="email"
-              placeholder="john@gmail.com"
+              placeholder="Email"
               className="ring-2 ring-gray-300 rounded-md p-4"
               onChange={(e) => setEmail(e.target.value)}
             />
@@ -190,10 +192,9 @@ const LoginPage = () => {
           </div>
         )}
         <button
-          className="bg-lama text-white p-2 rounded-md disabled:bg-pink-200 disabled:cursor-not-allowed"
-          disabled={isLoading}
+          className="bg-lama text-white p-2 rounded-md disabled:bg-pink-200 disabled:cursor-not-allowed hover:text-black"
         >
-          {isLoading ? "Loading..." : buttonTitle}
+          Submit
         </button>
         {error && <div className="text-red-600">{error}</div>}
         {mode === MODE.LOGIN && (
@@ -209,7 +210,7 @@ const LoginPage = () => {
             className="text-sm underline cursor-pointer"
             onClick={() => setMode(MODE.LOGIN)}
           >
-            Have and account?
+            Already have an account?
           </div>
         )}
         {mode === MODE.RESET_PASSWORD && (
@@ -217,7 +218,7 @@ const LoginPage = () => {
             className="text-sm underline cursor-pointer"
             onClick={() => setMode(MODE.LOGIN)}
           >
-            Go back to Login
+            Log In
           </div>
         )}
         {message && <div className="text-green-600 text-sm">{message}</div>}
