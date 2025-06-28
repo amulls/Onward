aebd04d1809b696cbfe563ae50f26d3a5c3d1607
amulls <amanda.l.mullings@gmail.com>
2025-06-28 18:13:53 -0400

    Update page.tsx

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -22,11 +22,11 @@ const SuccessPage = () => {
   }, [orderId, router]);
 
   return (
-    <div className="flex flex-col gap-6 items-center justify-center h-[calc(100vh-180px)]">
+    <div className="flex flex-col gap-6 items-center justify-center h-100">
       <Confetti width={2000} height={1000} />
       <h1 className="text-6xl text-green-700">Successful</h1>
       <h2 className="text-xl font-medium">
-        We sent the invoice to your e-mail
+        We sent you a confirmation email of your order.
       </h2>
       <h3 className="">You are being redirected to the order page...</h3>
     </div>
