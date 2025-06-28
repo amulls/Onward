e3124b78762c8e68dfcff4b7e7aaf9e110b30279
amulls <amanda.l.mullings@gmail.com>
2025-06-28 18:21:55 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-// "use client";
-
-import CategoryList from "@/components/CategoryList";
+import Bundles from "@/components/Bundles";
 import ProductList from "@/components/ProductList";
 import Skeleton from "@/components/Skeleton";
 import Slider from "@/components/Slider";
@@ -11,34 +9,11 @@ import { Suspense, useContext, useEffect } from "react";
 
 const HomePage = async () => {
 
-  // TEST (FETCHING ON THE CLIENT COMPONENT)
-
-  // const wixClient = useWixClient()
-
-  // useEffect(() => {
-  //   const getProducts = async () => {
-  //     const res = await wixClient.products.queryProducts().find();
-
-  //     console.log(res)
-  //   };
-
-  //   getProducts();
-  // }, [wixClient]);
-  
-
-  // TEST (FETCHING ON THE SERVER COMPONENT)
-
-  // const wixClient = await wixClientServer();
-
-  // const res = await wixClient.products.queryProducts().find();
-
-  // console.log(res);
-
   return (
     <div className="">
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-2xl">Featured Products</h1>
+        <h1 className="text-2xl text-semibold">Best Sellers</h1>
         <Suspense fallback={<Skeleton />}>
           <ProductList
             categoryId={process.env.FEATURED_PRODUCTS_FEATURED_CATEGORY_ID!}
@@ -46,21 +21,11 @@ const HomePage = async () => {
           />
         </Suspense>
       </div>
+
       <div className="mt-24">
-        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
-          Categories
-        </h1>
+        <h1 className="text-2xl text-semibold px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">The Goods</h1>
         <Suspense fallback={<Skeleton />}>
-          <CategoryList />
-        </Suspense>
-      </div>
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-2xl">New Products</h1>
-        <Suspense fallback={<Skeleton />}>
-          <ProductList
-            categoryId={process.env.FEATURED_PRODUCTS_NEW_CATEGORY_ID!}
-            limit={4}
-          />
+          <Bundles />
         </Suspense>
       </div>
     </div>
